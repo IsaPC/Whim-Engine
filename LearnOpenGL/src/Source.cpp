143c6ffd9e08dc808ce31742189c1e4b2f43aff0
IsaPC <isaacceff@gmail.com>
2020-06-19 13:46:31 +1000

    pulsing green triangle

--- LearnOpenGL/src/Source.cpp
+++ LearnOpenGL/src/Source.cpp
@@ -30,7 +30,6 @@ out vec4 vertexColor; // specify a color output to the fragment shader
 void main()
 {
     gl_Position = vec4(aPos, 1.0); // see how we directly give a vec3 to vec4's constructor
-    vertexColor = vec4(0.5, 0.0, 0.0, 1.0); // set the output variable to a dark-red color
 }
 )";
 
@@ -38,11 +37,12 @@ const char* fragmentShaderSource = R"(
 #version 330 core
 out vec4 FragColor;
 
-in vec4 vertexColor; // the input variable from the vertex shader (same name and same type)  
+
+uniform vec4 ourColor; // we set this variable in the OpenGL code.
 
 void main()
 {
-    FragColor = vertexColor;
+    FragColor = ourColor;
 }
 //)";
 
@@ -192,6 +192,13 @@ int main()
 
         // draw our first triangle
         glUseProgram(program);
+
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(program, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time 
