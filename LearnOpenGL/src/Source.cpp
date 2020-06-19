a015172f8338565aa3474654ff8f80035c8969dc
IsaPC <isaacceff@gmail.com>
2020-06-19 13:36:13 +1000

    implemented raw strings for GLSL scripts

--- LearnOpenGL/src/Source.cpp
+++ LearnOpenGL/src/Source.cpp
@@ -12,18 +12,40 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
+//const char* vertexShaderSource = R"(
+//#version 330 core
+//layout (location = 0) in vec3 aPos;
+//void main()
+//{
+//   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+//}
+//)";
+
+const char* vertexShaderSource = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos; // the position variable has attribute position 0
+  
+out vec4 vertexColor; // specify a color output to the fragment shader
+
+void main()
+{
+    gl_Position = vec4(aPos, 1.0); // see how we directly give a vec3 to vec4's constructor
+    vertexColor = vec4(0.5, 0.0, 0.0, 1.0); // set the output variable to a dark-red color
+}
+)";
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+
+in vec4 vertexColor; // the input variable from the vertex shader (same name and same type)  
+
+void main()
+{
+    FragColor = vertexColor;
+}
+//)";
+
 
 int createShader(unsigned int type, std::string shaderScript) {
     // GL_VERTEX_SHADER, GL_FRAGMENT_SHADER
